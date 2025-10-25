f58199285ff1e657269502c435aecc10a7030d63
Maple <wjxa20152015@gmail.com>
2025-10-25 18:48:35 +0800

    fix build

--- build_aar.fish
+++ build_aar.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
 set -x ANDROID_NDK_HOME /home/soda/Android/Sdk/ndk/27.0.12077973
-gomobile bind -target=android -androidapi 21 -o flutter_gui/libs/core.aar github.com/soda92/vpn-share-tool/mobile
+gomobile bind -target=android -androidapi 21 -o flutter_gui/android/libs/core.aar github.com/soda92/vpn-share-tool/mobile
