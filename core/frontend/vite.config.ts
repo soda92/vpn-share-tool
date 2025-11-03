d7152f5f3c51085cb85bb0b43450c116a16a9e15
Maple <wjxa20152015@gmail.com>
2025-11-03 13:12:04 +0800

    mv

--- core/frontend/vite.config.ts
+++ core/frontend/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/url-decoder/', // Add this line
+  base: '/debug/',
   plugins: [
     vue(),
     vueDevTools(),
