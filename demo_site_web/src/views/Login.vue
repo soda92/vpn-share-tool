4272d1f11042d6ed6b0a7a2787eb41ad7f74ac70
Maple <wjxa20152015@gmail.com>
2026-01-15 13:16:12 +0800

    add test page

--- demo_site_web/src/views/Login.vue
+++ demo_site_web/src/views/Login.vue
@@ -7,6 +7,14 @@
       <button type="submit">Login</button>
     </form>
     <dt id="dd65-ddheader" onclick="Ehr.phisIndex()">健康档案</dt>
+
+    <div style="margin-top: 20px; border-top: 1px solid #ccc; padding-top: 10px;">
+      <h3>Debug Links</h3>
+      <ul>
+        <li><a href="/?simple=value&num=123&bool=true">Simple Params</a></li>
+        <li><a href="/?data=%7B%22id%22%3A1%2C%22name%22%3A%22test%22%7D&list=1&list=2">JSON & Array Params</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
